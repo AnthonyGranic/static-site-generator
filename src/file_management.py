1730df81526a02f5d5796ef146a69c1e6c8777c4
AnthonyGranic <granicanthony@gmail.com>
2025-04-11 23:10:52 -0400

    deploy to github pages

--- src/file_management.py
+++ src/file_management.py
@@ -6,32 +6,28 @@ from markdowntohtml import extract_title, markdown_to_html_node
 
 
 def copy_static_to_public():
-    if os.path.exists("./public"):
-        shutil.rmtree("./public")
+    if os.path.exists("./docs"):
+        shutil.rmtree("./docs")
 
-    os.mkdir("./public")
+    os.mkdir("./docs")
 
-    copy_all_files_to_dir("./static/", "./public/")
+    copy_all_files_to_dir("./static/", "./docs/")
 
 
-def copy_all_files_to_dir(source_dir, target_dir, subdir=""):
+def copy_all_files_to_dir(source_dir, target_dir):
 
-    current_dir = os.path.join(source_dir, subdir)
-    target_subdir = os.path.join(target_dir, subdir)
-
-    os.makedirs(target_subdir, exist_ok=True)
-
-    for item in os.listdir(current_dir):
-        source_path = os.path.join(current_dir, item)
-        target_path = os.path.join(target_subdir, item)
+    for item in os.listdir(source_dir):
+        source_path = os.path.join(source_dir, item)
+        target_path = os.path.join(target_dir, item)
 
         if os.path.isfile(source_path):
             shutil.copy2(source_path, target_path)
         elif os.path.isdir(source_path):
-            copy_all_files_to_dir(source_dir, target_dir, os.path.join(subdir, item))
+            os.makedirs(target_path, exist_ok=True)
+            copy_all_files_to_dir(source_path, target_path)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     md_text = ""
@@ -51,16 +47,19 @@ def generate_page(from_path, template_path, dest_path):
 
     template_with_title = template_text.replace(r"{{ Title }}", title)
     template_with_content = template_with_title.replace(r"{{ Content }}", html_text)
+    template_with_basepath = template_with_content.replace(
+        'href="/', f'href="{basepath}'
+    ).replace('src="/', f'src="{basepath}')
 
     dirname = os.path.dirname(dest_path)
     os.makedirs(dirname, exist_ok=True)
 
     with open(dest_path, "w") as dest:
-        dest.write(template_with_content)
+        dest.write(template_with_basepath)
         dest.close()
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
 
     for item in os.listdir(dir_path_content):
         source_path = os.path.join(dir_path_content, item)
@@ -69,8 +68,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(source_path) and source_path.endswith(".md"):
 
             html_target = target_path.replace("md", "html")
-            generate_page(source_path, template_path, html_target)
+            generate_page(source_path, template_path, html_target, basepath)
 
         elif os.path.isdir(source_path):
 
-            generate_pages_recursive(source_path, template_path, target_path)
+            generate_pages_recursive(source_path, template_path, target_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from file_management import copy_static_to_public, generate_pages_recursive
+import sys
 
 
 def main():
+    if len(sys.argv) != 2 and len(sys.argv) != 1:
+        raise Exception("usage is python3 main.py <root path>")
+
+    basepath = sys.argv[1] if len(sys.argv) == 2 else "/"
+
     copy_static_to_public()
-    generate_pages_recursive("./content/", "template.html", "./public/")
+    generate_pages_recursive("./content/", "template.html", "./docs/", basepath)
 
 
 if __name__ == "__main__":
